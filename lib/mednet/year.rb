3e7703aa95cb2cade1e2ef6501855b639d056a77
Doug Emery <emeryr@upenn.edu>
2015-04-04 15:03:47 -0400

    Parse feasts lazily; use better method names

--- lib/mednet/year.rb
+++ lib/mednet/year.rb
@@ -1,6 +1,5 @@
 module Mednet
   class Year
-
     attr_accessor :file
 
     MONTHS =  %w{
@@ -22,27 +21,40 @@ module Mednet
 
     DAY_REGEX = /name="\d+">(#{MONTHS.join('|')})\s+(\d{1,2})/
 
-    def initialize(file)
-      @file = file
+    def initialize file
+      @file  = file
+      @month = nil
+      @day   = nil
     end
 
-    def parse
-      feasts = []
-      day = nil
-      IO.foreach file do |line|
-        if line =~ DAY_REGEX
-          day = OpenStruct.new month: $1, day: $2
-        elsif line.strip =~ /^\s*<br>(.*)$/
-          s           = $1
-          feast       = Feast.new.parse s
-          feast.month = day.month
-          feast.day   = day.day
-          feast.line  = s
-          feasts << feast
+    def date
+      OpenStruct.new month: @month, day: @day
+    end
+
+    def each_feast_line
+      return enum_for(:each) unless block_given?
+
+      File.open file, 'r' do |f|
+        f.each do |line|
+          if line =~ DAY_REGEX
+            @month = $1
+            @day   = $2
+          elsif line =~ /^\s*<br>(.*)$/
+            yield line
+          end
         end
       end
-      feasts
     end
 
+    def each_feast
+      count = 0
+      each_feast_line do |feast_line|
+        feast       = Feast.new.parse feast_line
+        feast.month = date.month
+        feast.day   = date.day
+        feast.line  = feast_line
+        yield feast
+      end
+    end
   end
 end
